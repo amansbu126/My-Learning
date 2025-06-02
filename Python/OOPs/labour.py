3221801195a4818544e16155d618a8e795b778e7
amansbu126 <amansept22@gmail.com>
2025-06-03 00:57:44 +0530

    continuing

--- Python/OOPs/labour.py
+++ Python/OOPs/labour.py
@@ -6,8 +6,14 @@ class Labour:
         self.last_name=last_name
         self.wage=wage
 # if we add prefix __variable it will become private
-    def save_to_db(self,db_connection):
-        pass
+
+    def save_to_db(self):
+        query = "SELECT id from labours where lower(first_name)= %s AND lower(last_name) = %s"
+        result = self.crud.read_from_mysql(query,(self.first_name,self.last_name))
+
+        if result: #if labour already exists, return existing ID
+                logger.info(f"Labour already existes with ID: {result[0][0]}")
+                return result[0][0]
 
     def login(self):
         pass
